7b6e85c780dffa43e68af4d136353d72bfcce361
chloe-vincent03 <chloe.vincent03@edu.univ-fcomte.fr>
2025-10-17 15:16:51 +0200

    lien cloudflare

--- src/controllers/media.controller.js
+++ src/controllers/media.controller.js
@@ -1,9 +1,8 @@
+// src/controllers/media.controller.js
 const sqlite3 = require("sqlite3").verbose();
-const path = require("path");
+const dbPath = process.env.DB_PATH || "./database.db";
 
-const dbPath = path.join(__dirname, "../../database.db");
-
-function getAllMedia(req, res) {
+exports.getAllMedia = (req, res) => {
   const db = new sqlite3.Database(dbPath);
   db.all("SELECT * FROM media", (err, rows) => {
     if (err) {
@@ -13,6 +12,4 @@ function getAllMedia(req, res) {
     }
     db.close();
   });
-}
-
-module.exports = { getAllMedia };
+};
