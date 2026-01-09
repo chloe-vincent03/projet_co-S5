f1755abeb4886448027fffb75509ed47d8ff8065
noelie <noelie.talhouarn@edu.univ-fcomte.fr>
2026-01-09 10:26:29 +0100

    feat(contact) page contact

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="flex items-center justify-center px-4 py-10 lg:py-4 lg:pt-10">
 
-    <div class="bg-white shadow-lg rounded-xl w-full 
+    <div class="bg-white shadow-lg w-full 
       max-w-md p-6 lg:p-10 
       border border-blue-plumepixel">
 
    
-      <h1 class="text-3xl text-blue-plumepixel mb-8 font-[PlumePixel] text-center lg:text-left">
+      <h1 class="text-4xl text-blue-plumepixel mb-8 font-[PlumePixel] text-center lg:text-left">
         Connectez-vous !
       </h1>
 
       <form @submit.prevent="loginUser" class="space-y-5">
 
         <div>
-          <label class="block text-sm font-medium text-gray-700 mb-1">Adresse mail</label>
+          <label class="block text-sm font-medium text-black mb-1">Adresse mail</label>
           <input
             v-model="email"
             type="email"
             required
-            class="w-full px-4 py-2 border border-gray-300 rounded-lg 
+            class="w-full px-4 py-2 border border-gray-300 
               focus:ring-0 focus:ring-blue-plumepixel focus:border-blue-plumepixel outline-none"
           />
         </div>
 
         <div>
-          <label class="block text-sm font-medium text-gray-700 mb-1">Mot de passe</label>
+          <label class="block text-sm font-medium text-black mb-1">Mot de passe</label>
           <input
             v-model="password"
             type="password"
             required
-            class="w-full px-4 py-2 border border-gray-300 rounded-lg 
+            class="w-full px-4 py-2 border border-gray-300 
               focus:ring-0 focus:ring-blue-plumepixel focus:border-blue-plumepixel outline-none"
           />
         </div>
